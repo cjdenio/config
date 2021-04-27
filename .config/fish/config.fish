ee432791ace0adb54584c69ae6fcb03cae3667a4
Caleb Denio <cjdenio44@gmail.com>
2021-04-26 22:28:16 -0400

    Update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -74,3 +74,6 @@ status --is-interactive; and source (rbenv init -|psub)
 
 # Environment variables
 set -x EDITOR nvim
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
